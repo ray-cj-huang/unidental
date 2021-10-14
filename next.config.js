d437acb7648f1e8f5330265761c3860601903a82
Ray Huang <rayhuang.cj@gmail.com>
2021-10-13 23:51:30 -0700

    config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
-module.exports {
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
 }
+
+module.exports = nextConfig
