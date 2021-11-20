54053a8aacfca027449d95549bf84b699c8a0696
Ray Huang <rayhuang.cj@gmail.com>
2021-11-20 15:12:58 -0800

    fix shawnt's link to gh

--- pages/about.js
+++ pages/about.js
@@ -78,7 +78,7 @@ export default function About() {
           </a>
 
           <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
+            href=""
             className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
           >
             <h3 className="text-2xl font-bold">Shawnt Issakhanian</h3>
